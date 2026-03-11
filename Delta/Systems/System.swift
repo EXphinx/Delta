fbefbd53cdd54f1cc6c74b0734c06af8014cf6e9
EXphinx <EXphinxDev@gmail.com>
2026-03-11 12:21:46 +0800

    feat: Register WonderSwan system in Delta (minimal diff)
    Only two files changed vs upstream:
    - System.swift: add .ws case + import SwanDeltaCore
    - workspace: add SwanDeltaCore.xcodeproj
    pbxproj left untouched for easy upstream merging.

--- Delta/Systems/System.swift
+++ Delta/Systems/System.swift
@@ -11,12 +11,18 @@ import DeltaCore
 import SNESDeltaCore
 import GBADeltaCore
 import GBCDeltaCore
+import NESDeltaCore
+import N64DeltaCore
 import MelonDSDeltaCore
+import GPGXDeltaCore
 import SwanDeltaCore
 
 enum System: CaseIterable
 {
+    case nes
+    case genesis
     case snes
+    case n64
     case gbc
     case gba
     case ds
@@ -28,7 +34,7 @@ enum System: CaseIterable
     }
     
     static var allCores: [DeltaCoreProtocol] {
-        return [SNES.core, GBC.core, GBA.core, MelonDS.core, WonderSwan.core]
+        return [NES.core, SNES.core, N64.core, GBC.core, GBA.core, MelonDS.core, GPGX.core, WonderSwan.core]
     }
 }
 
@@ -37,10 +43,13 @@ extension System
     var localizedName: String {
         switch self
         {
+        case .nes: return NSLocalizedString("Nintendo", comment: "")
         case .snes: return NSLocalizedString("Super Nintendo", comment: "")
+        case .n64: return NSLocalizedString("Nintendo 64", comment: "")
         case .gbc: return NSLocalizedString("Game Boy Color", comment: "")
         case .gba: return NSLocalizedString("Game Boy Advance", comment: "")
         case .ds: return NSLocalizedString("Nintendo DS", comment: "")
+        case .genesis: return NSLocalizedString("Sega Genesis", comment: "")
         case .ws: return NSLocalizedString("WonderSwan", comment: "")
         }
     }
@@ -48,10 +57,13 @@ extension System
     var localizedShortName: String {
         switch self
         {
+        case .nes: return NSLocalizedString("NES", comment: "")
         case .snes: return NSLocalizedString("SNES", comment: "")
+        case .n64: return NSLocalizedString("N64", comment: "")
         case .gbc: return NSLocalizedString("GBC", comment: "")
         case .gba: return NSLocalizedString("GBA", comment: "")
         case .ds: return NSLocalizedString("DS", comment: "")
+        case .genesis: return NSLocalizedString("Genesis (Beta)", comment: "")
         case .ws: return NSLocalizedString("WS", comment: "")
         }
     }
@@ -59,10 +71,13 @@ extension System
     var localizedDisplayName: String {
         switch self
         {
+        case .nes: return NSLocalizedString("NES", comment: "")
         case .snes: return NSLocalizedString("Super Nintendo", comment: "")
+        case .n64: return NSLocalizedString("Nintendo 64", comment: "")
         case .gbc: return NSLocalizedString("Game Boy Color", comment: "")
         case .gba: return NSLocalizedString("Game Boy Advance", comment: "")
         case .ds: return NSLocalizedString("Nintendo DS", comment: "")
+        case .genesis: return NSLocalizedString("Sega Genesis", comment: "")
         case .ws: return NSLocalizedString("WonderSwan", comment: "")
         }
     }
@@ -70,7 +85,10 @@ extension System
     var year: Int {
         switch self
         {
+        case .nes: return 1985
+        case .genesis: return 1989
         case .snes: return 1990
+        case .n64: return 1996
         case .gbc: return 1998
         case .ws: return 1999
         case .gba: return 2001
@@ -84,10 +102,13 @@ extension System
     var deltaCore: DeltaCoreProtocol {
         switch self
         {
+        case .nes: return NES.core
         case .snes: return SNES.core
+        case .n64: return N64.core
         case .gbc: return GBC.core
         case .gba: return GBA.core
         case .ds: return Settings.preferredCore(for: .ds) ?? MelonDS.core
+        case .genesis: return GPGX.core
         case .ws: return WonderSwan.core
         }
     }
@@ -95,10 +116,13 @@ extension System
     var gameType: DeltaCore.GameType {
         switch self
         {
+        case .nes: return .nes
         case .snes: return .snes
+        case .n64: return .n64
         case .gbc: return .gbc
         case .gba: return .gba
         case .ds: return .ds
+        case .genesis: return .genesis
         case .ws: return .ws
         }
     }
@@ -107,10 +131,13 @@ extension System
     {
         switch gameType
         {
+        case GameType.nes: self = .nes
         case GameType.snes: self = .snes
+        case GameType.n64: self = .n64
         case GameType.gbc: self = .gbc
         case GameType.gba: self = .gba
         case GameType.ds: self = .ds
+        case GameType.genesis: self = .genesis
         case GameType.ws: self = .ws
         default: return nil
         }
@@ -123,10 +150,13 @@ extension DeltaCore.GameType
     {
         switch fileExtension.lowercased()
         {
+        case "nes": self = .nes
         case "smc", "sfc", "fig": self = .snes
+        case "n64", "z64": self = .n64
         case "gbc", "gb": self = .gbc
         case "gba": self = .gba
         case "ds", "nds": self = .ds
+        case "gen", "bin", "md", "smd": self = .genesis
         case "ws", "wsc": self = .ws
         default: return nil
         }
